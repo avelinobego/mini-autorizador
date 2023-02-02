133c6246591f04b34f515c74411fac4998587f53
Avelino Bego <avelino.bego@gmail.com>
2023-02-01 21:44:04 -0300

    Adição de Swagger

--- java/mini-autorizador/src/main/java/br/com/chequecardapio/App.java
+++ java/mini-autorizador/src/main/java/br/com/chequecardapio/App.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
+@EnableSwagger2 //Utilize-se do endereço http://localhost:8080/swagger-ui/index.html
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
